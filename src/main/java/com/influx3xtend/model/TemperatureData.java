ab50252d30c1a6a8477947d45b286d4834e6e34a
panda00hi <2127721837@qq.com>
2025-07-25 16:00:11 +0800

    fix: model toString

--- src/main/java/com/influx3xtend/model/TemperatureData.java
+++ src/main/java/com/influx3xtend/model/TemperatureData.java
@@ -2,6 +2,7 @@ package com.influx3xtend.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 /**
  * @author panda00hi
@@ -10,6 +11,7 @@ import lombok.EqualsAndHashCode;
 // todo 后续优化项引入注解。目前先在各自的实现引擎中自行解析赋值
 //  1. 识别与数据库中字段的命名映射关系；
 //  2. 标识类型，如tag、field等；
+@ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class TemperatureData extends PointBase {
